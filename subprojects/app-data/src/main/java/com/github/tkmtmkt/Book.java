dda7971c115ed3c9b6059084cfd4d69794184e2b
Takamatsu Makoto <tkmtmkt@gmail.com>
2013-08-11 20:54:17 +0900

    マルチプロジェクトのみ

---
+++ subprojects/app-data/src/main/java/com/github/tkmtmkt/Book.java
@@ -0,0 +1,32 @@
+package com.github.tkmtmkt;
+
+import com.tangosol.io.pof.annotation.Portable;
+import com.tangosol.io.pof.annotation.PortableProperty;
+
+@Portable
+public class Book
+{
+    @PortableProperty
+    private String title;
+
+    @PortableProperty
+    private String author;
+
+    public String getTitle() {
+        return title;
+    }
+
+    void setTitle(final String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(final String author) {
+        this.author = author;
+    }
+}
+
+// vim: set ts=4 sw=4 et:
---
+++ subprojects/app-main/src/main/java/com/github/tkmtmkt/Main.java
@@ -0,0 +1,16 @@
+package com.github.tkmtmkt;
+
+import com.tangosol.net.CacheFactory;
+import com.tangosol.net.NamedCache;
+
+public class Main
+{
+    public static void main(String[] args) {
+        NamedCache hoge = CacheFactory.getCache("hoge");
+        Book book = new Book();
+        book.setTitle("Scalaで学ぶ関数脳入門");
+        book.setAuthor("株式会社テクノロジックアート");
+        hoge.put("ISBN978-4-7741-4436-8", book);
+    }
+}
+// vim: set ts=4 sw=4 et:
